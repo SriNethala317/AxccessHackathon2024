d3383ecefc1ce44586256b174f70ffb4943e6dbc
SriNethala317 <srinethala123@gmail.com>
2024-02-17 22:56:37 -0600

    Completed BottomNavBar component

--- AxccessHackathon2024/src/components/BottomNavBar.js
+++ AxccessHackathon2024/src/components/BottomNavBar.js
@@ -3,6 +3,7 @@ import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import { Entypo } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Fontisto } from '@expo/vector-icons';
+import { useFonts, Homenaje_400Regular } from "@expo-google-fonts/dev";
 
 const BottomNavBar = () => {
   return (
@@ -10,15 +11,15 @@ const BottomNavBar = () => {
       <View style={styles.navContainer}>
         <View style={styles.tabGroup}>
           <Entypo name="star" size={40} color="#C9184A" />
-          <Text>Quick View</Text>
+          <Text style={styles.tabText}>Quick View</Text>
         </View>
         <View style={styles.tabGroup}>
           <MaterialCommunityIcons name="chart-line-stacked" size={40} color="#5A1327" />  
-          <Text>Chart View</Text>
+          <Text style={styles.tabText}>Chart View</Text>
         </View>
         <View style={styles.tabGroup}>
           <Fontisto name="smiley" size={40} color="#5A1327" />
-          <Text>Help</Text>  
+          <Text style={styles.tabText}>Help</Text>  
         </View>
       </View>
 
@@ -42,7 +43,13 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   tabGroup: {
-    
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  tabText: {
+    paddingTop: 3,
+    fontFamily: 'Homenaje_400Regular'
   }
 });
 
